98f103db055ed3473c22f110113b9ae7e80d1d4c
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-01 21:30:38 +0700

    Introduction to the Python issubset() method

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 if __name__ == "__main__":
-  print("Python issubset")
+  # Introduction to the Python issubset() method
+
+  """
+  set_a.issubset(set_b)
+  """
+
+  numbers = {1, 2, 3, 4, 5}
+  scores = {1, 2, 3}
+  print(numbers)
+  print(scores)
+  print(scores.issubset(numbers))
+
+  numbers = {1, 2, 3, 4, 5}
+  print(numbers)
+  print(numbers.issubset(numbers))
+
+  numbers = {1, 2, 3, 4, 5}
+  scores = {1, 2, 3}
+  print(numbers)
+  print(scores)
+  print(numbers.issubset(scores))
